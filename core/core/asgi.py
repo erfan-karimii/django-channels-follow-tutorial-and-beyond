f62aa27a825f9424f1e4dc892703bb9656a24f0d
erfan <h410mi3@gmail.com>
2023-03-13 00:07:23 +0330

    writing my first consumer

--- core/core/asgi.py
+++ core/core/asgi.py
@@ -9,15 +9,18 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 
 import os
 
-from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter
-from django.core.asgi import get_asgi_application
-
+from channels.auth import AuthMiddlewareStack
+from django.core.asgi import get_asgi_application 
+from channels.security.websocket import AllowedHostsOriginValidator
+from channels.routing import ProtocolTypeRouter , URLRouter
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+import chat.routing
 
 django_asgi_app  = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app ,
-    # Just HTTP for now. (We can add other protocols later.)
+    "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns))
+    ),
 })
