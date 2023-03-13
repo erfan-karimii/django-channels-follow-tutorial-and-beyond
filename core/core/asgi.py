62b7776a004773443a2a7974b830d7731e7ab23e
erfan <h410mi3@gmail.com>
2023-03-13 09:38:23 +0330

    finish basic chat server

--- core/core/asgi.py
+++ core/core/asgi.py
@@ -6,15 +6,15 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
-
 import os
 
 from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application 
 from channels.security.websocket import AllowedHostsOriginValidator
 from channels.routing import ProtocolTypeRouter , URLRouter
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 import chat.routing
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 django_asgi_app  = get_asgi_application()
 
