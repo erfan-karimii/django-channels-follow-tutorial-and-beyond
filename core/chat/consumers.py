dfa391fb53a6216743ca43f2470b4b0276fd33d4
erfan <h410mi3@gmail.com>
2023-03-14 19:03:54 +0330

    working on saving message
    save messages to database after user send so if user reload page or leave the room , messages will not disappear.

--- core/chat/consumers.py
+++ core/chat/consumers.py
@@ -1,21 +1,27 @@
 import json
 import random
 from channels.generic.websocket import AsyncWebsocketConsumer
-
+from channels.db import database_sync_to_async
+from .models import Chat , ChatRoom
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = "chat_%s" % self.room_name
-        self.user_name = self.scope['user'].username + " :"
-        if self.scope['user'].is_anonymous:
-            self.user_name = 'anouser' + str(random.randint(0,100))+" :"
+        print(self.scope['cookies'])
+        if self.scope['user'].is_authenticated:
+            self.user_name = self.scope['user'].username + " : "
+        elif self.scope['cookies']:
+            self.user_name = self.scope['cookies']['anonymoususer'] + " : "
         # Join room group
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
 
         await self.accept()
 
     async def disconnect(self, close_code):
+        await self.channel_layer.group_send(
+            self.room_group_name, {"type": "chat_message", "message": 'left.','username': self.user_name}
+        )
         # Leave room group
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
@@ -23,6 +29,19 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
+
+        # Find room object
+        room = await database_sync_to_async(ChatRoom.objects.get)(name=self.room_name)
+
+
+        # Creat New Chat
+        if self.scope['user'].is_authenticated:
+            user = self.scope['user']
+        else :
+            user = None
+        chat = Chat(content= message,user = user,room=room)
+        await database_sync_to_async(chat.save)()
+
 
         # Send message to room group
         await self.channel_layer.group_send(
@@ -34,6 +53,6 @@ class ChatConsumer(AsyncWebsocketConsumer):
         message = event["message"]
         username = event["username"]
         if self.user_name == username:
-            username = 'ME :'
+            username = 'ME : '
         # Send message to WebSocket
         await self.send(text_data=json.dumps({"message":  username + message}))
--- core/chat/views.py
+++ core/chat/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
-
+from .models import ChatRoom , Chat
 # Create your views here.
 
 def index(request):
     return render(request, "chat/index.html")
 
 def room(request, room_name):
-    return render(request, "chat/room.html", {"room_name": room_name})
+    room = ChatRoom.objects.filter(name=room_name).first()
+    chats = []
+    if room:
+        chats = Chat.objects.filter(room=room)
+    else:
+        room = ChatRoom(name=room_name)
+        room.save()
+
+    return render(request, "chat/room.html", {"room_name": room_name,'chats':chats})
