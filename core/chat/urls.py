c883bb1555e72da3c97b8fc60224e185eed20a6e
erfan <h410mi3@gmail.com>
2023-03-30 22:11:02 +0330

    sign-in & sign-up & logout finished

--- core/chat/urls.py
+++ core/chat/urls.py
@@ -7,5 +7,7 @@ app_name= 'chat'
 urlpatterns = [
     path("", views.index, name="index"),
     path("sign-in/", views.sign_in, name="sign-in"),
+    path("sign-up/", views.sign_up, name="sign-up"),
+    path("sign-out/", views.sign_out, name="sign-out"),
     path("<str:room_name>/", views.room, name="room"),
 ]
--- core/chat/views.py
+++ core/chat/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
+from django.contrib.auth import authenticate, login ,logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm ,AuthenticationForm
+from django.contrib.auth.hashers import make_password 
+from django.contrib.auth.models import User
 from .models import ChatRoom , Chat
 # Create your views here.
 
@@ -17,5 +22,28 @@ def room(request, room_name):
     return render(request, "chat/room.html", {"room_name": room_name,'chats':chats})
 
 def sign_in(request):
-    
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request,user)
+            return redirect('chat:index')
+        else:
+            print(form.errors)
     return render(request,'chat/signin.html',{})
+
+def sign_up(request):
+    error_messages = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('chat:index')
+        else:
+            error_messages = form.errors.values
+    return render(request,'chat/signup.html',{'error_messages':error_messages})
+
+@login_required(login_url='/sign-in/')
+def sign_out(request):
+    logout(request)
+    return redirect('chat:index')
